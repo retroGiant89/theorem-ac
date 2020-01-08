76467a52f869e12362555566a760f13283bca780
Lee Dykes <lee.dykes@metova.com>
2020-01-08 11:22:11 -0600

    device scaffolded

---
+++ db/migrate/20200108171842_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :devices do |t|
+      t.string :serial_number
+      t.integer :firmware_version
+
+      t.timestamps
+    end
+  end
+end
