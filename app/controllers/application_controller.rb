23c594ba49f5edfde944b84392a5bf0bd73e6280
Lee Dykes <lee.dykes@metova.com>
2020-01-10 12:31:14 -0600

    Notification alerts on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_notifications_stats
 
   protected
+
+  def set_notifications_stats
+    return unless admin_signed_in?
+    @warning_count = Notification.active.warning.count
+    @danger_count = Notification.active.danger.count
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:invite, keys: [:first_name, :last_name])
