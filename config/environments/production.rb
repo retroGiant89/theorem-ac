1de753a1e6f3e6c369c35918977a0821c3d9c2f8
Lee Dykes <lee.d.dykes@gmail.com>
2020-01-10 23:20:25 -0600

    set from email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://theorem-ac.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://theorem-ac.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
